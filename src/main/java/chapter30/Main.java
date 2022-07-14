fbd8e12b2690435fc9a66844a61e4c5bd8edc0dc
ahah525 <ahah525@naver.com>
2022-07-14 10:25:39 +0900

    30.size 함수 구현 및 코드 수정

--- src/main/java/chapter30/Main.java
+++ src/main/java/chapter30/Main.java
@@ -3,31 +3,22 @@ package chapter30;
 public class Main {
     public static void main(String[] args) {
         ArrayList ar = new ArrayList();
-        ar.add(100);
-        ar.add(200);
-        ar.add(300);
-        ar.add(400);
 
-        int value = ar.get(0);
-        System.out.println(value);
-        // 출력 : 100
+        for ( int i = 0; i < 100; i++ ) {
+            ar.add((i + 1) * 10);
+        }
 
-        value = ar.get(1);
-        System.out.println(value);
-        // 출력 : 200
+        int ar_size = ar.size(); // ar_size의 값은 100 이어야 합니다.
 
-        value = ar.get(2);
-        System.out.println(value);
-        // 출력 : 300
-
-        value = ar.get(3);
-        System.out.println(value);
-        // 출력 : 400
+        for ( int i = 0; i < ar_size; i++ ) {
+            int value = ar.get(i);
+            System.out.println(value);
+        }
     }
 }
 class ArrayList {
     int[] arr;
-    int idx = 0;    // 넣어야할 인덱스 번호
+    int idx = -1;    // 마지막 인덱스 번호
 
     public ArrayList() {
         arr = new int[3];   // 여기변경불가
@@ -38,7 +29,7 @@ class ArrayList {
     }
 
     public void add(int v) {
-        if(idx >= arr.length) {
+        if(idx + 1 >= arr.length) {
             // 기존 배열의 2배 크기인 새로운 배열
             int[] newArr = new int[arr.length * 2];
             // 새로운 배열로 기존 배열 복사하기
@@ -47,6 +38,10 @@ class ArrayList {
             }
             arr = newArr;
         }
-        arr[idx++] = v; // 배열에 삽입
+        arr[++idx] = v; // 배열에 삽입
+    }
+
+    public int size() {
+        return idx + 1;
     }
 }
