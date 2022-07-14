25381238d8e1a10538a0a10954b96e101b2f2d96
ahah525 <ahah525@naver.com>
2022-07-14 10:09:24 +0900

    배열의 길이가 필요시 자동 증가(교체) 구현하기

--- src/main/java/chapter30/Main.java
+++ src/main/java/chapter30/Main.java
@@ -6,6 +6,7 @@ public class Main {
         ar.add(100);
         ar.add(200);
         ar.add(300);
+        ar.add(400);
 
         int value = ar.get(0);
         System.out.println(value);
@@ -18,17 +19,34 @@ public class Main {
         value = ar.get(2);
         System.out.println(value);
         // 출력 : 300
+
+        value = ar.get(3);
+        System.out.println(value);
+        // 출력 : 400
     }
 }
 class ArrayList {
-    int[] arr = new int[100];
-    int idx = 0;
+    int[] arr;
+    int idx = 0;    // 넣어야할 인덱스 번호
+
+    public ArrayList() {
+        arr = new int[3];   // 여기변경불가
+    }
 
     public int get(int index) {
         return arr[index];
     }
 
     public void add(int v) {
-        arr[idx++] = v;
+        if(idx >= arr.length) {
+            // 기존 배열의 2배 크기인 새로운 배열
+            int[] newArr = new int[arr.length * 2];
+            // 새로운 배열로 기존 배열 복사하기
+            for (int i = 0; i < arr.length; i++) {
+                newArr[i] = arr[i];
+            }
+            arr = newArr;
+        }
+        arr[idx++] = v; // 배열에 삽입
     }
 }
