86947ca6dfc26fcb2a368a3875b9a978b9ec2b7c
ahah525 <ahah525@naver.com>
2022-07-14 09:37:30 +0900

    30.ArrayList-add,get 함수 구현

--- src/main/java/chapter30/Main.java
+++ src/main/java/chapter30/Main.java
@@ -6,12 +6,29 @@ public class Main {
         ar.add(100);
         ar.add(200);
         ar.add(300);
+
+        int value = ar.get(0);
+        System.out.println(value);
+        // 출력 : 100
+
+        value = ar.get(1);
+        System.out.println(value);
+        // 출력 : 200
+
+        value = ar.get(2);
+        System.out.println(value);
+        // 출력 : 300
     }
 }
 class ArrayList {
+    int[] arr = new int[100];
+    int idx = 0;
 
+    public int get(int index) {
+        return arr[index];
+    }
 
     public void add(int v) {
-
+        arr[idx++] = v;
     }
 }
