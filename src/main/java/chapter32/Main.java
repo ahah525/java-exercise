dae4712e1c2b3fd3b8e3784fbf868cb826901d75
ahah525 <ahah525@naver.com>
2022-07-18 10:02:08 +0900

    chapter32.ArrayList에 제너릭 적용하기

--- src/main/java/chapter32/Main.java
+++ src/main/java/chapter32/Main.java
@@ -1,46 +1,86 @@
 package chapter32;
 
-//code.oa.gg/java8/1350
-// 문제 : 제너릭을 사용해서 중복을 제거해주세요.
+//code.oa.gg/java8/1352
+// 문제 : 아래가 실행되도록 해주세요.
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        저장소<Integer> a저장소 = new 저장소<>();
+        ArrayList<Integer> ar1 = new ArrayList<Integer>();
 
-        a저장소.setData(30);
-        int a = a저장소.getData();
+        ar1.add(10);
+        ar1.add(20);
+        System.out.println(ar1.get(0) + ar1.get(1));
+        // 출력 : 30
 
-        System.out.println(a);
+        ArrayList<Boolean> ar2 = new ArrayList<Boolean>();
 
-        저장소<Double> b저장소 = new 저장소<>();
-
-        b저장소.setData(5.5);
-        double b = b저장소.getData();
-
-        System.out.println(b);
-
-
-        저장소<사과> c저장소 = new 저장소<>();
-
-        c저장소.setData(new 사과());
-        사과 c = c저장소.getData();
-
-        System.out.println(c);
+        ar2.add(true);
+        ar2.add(true);
+        if ( ar2.get(0) && ar2.get(1) ) {
+            System.out.println("둘다 참 입니다.");
+        }
+        // 출력 : 둘다 참 입니다.
     }
 }
 
-class 저장소<T> {
-    Object data;
+class ArrayList<T> {
+    Object[] datas;
+    int lastIndex = -1;
 
-    T getData() {
-        return (T)data;
+    ArrayList() {
+        datas = new Object[3]; // 이 부분은 수정할 수 없습니다.
     }
 
-    void setData(Object inputedData) {
-        this.data = inputedData;
+    void add(Object data, int index) {
+        lastIndex++;
+        extendDatasSizeIfNeed();
+
+        // 구현시작
+        // 맨 뒤의 손님을 끝쪽으로 한칸 이동시킨다.
+        // 맨 뒤에서 2번째 손님을 끝쪽으로 한칸 이동시킨다.
+        // 맨 뒤에서 3번째 손님을 끝쪽으로 한칸 이동시킨다.
+        // 맨 뒤에서 index번째 손님을 끝쪽으로 한칸 이동시킨다.
+        for ( int i = lastIndex - 1; i > index - 1; i-- ) {
+            datas[i + 1] = datas[i];
+        }
+
+        // 구현끝
+
+        datas[index] = data;
+    }
+
+    void add(Object data) {
+        lastIndex++;
+
+        extendDatasSizeIfNeed();
+
+        datas[lastIndex] = data;
+    }
+
+    void extendDatasSizeIfNeed() {
+        if ( lastIndex >= datas.length ) {
+            // 확장공사
+            // 기존버스 버리고 새 버스로 연결!!
+            // datas 이 녀석이 기존 버스를 버리고 새 버스를 가리켜야 합니다.
+
+            // 새 버스 생성
+            Object[] newArr = new Object[datas.length * 2];
+
+            // 기존 버스(배열)를 버리기 전에 버스에 있던 승객들을 새 버스로 옮긴다.
+            for ( int i = 0; i < datas.length; i++ ) {
+                newArr[i] = datas[i];
+            }
+
+            datas = newArr;
+        }
+    }
+
+    T get(int index) {
+        return (T)datas[index];
+    }
+
+    int size() {
+        return lastIndex + 1;
     }
 }
 
-class 사과 {
-
-}
