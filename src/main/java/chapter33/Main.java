a58faec8a4208351f034b60e1d515d3f7e9a122e
ahah525 <ahah525@naver.com>
2022-07-18 11:05:31 +0900

    chapter33.HashMap 만들기1

---
+++ src/main/java/chapter33/Main.java
@@ -0,0 +1,18 @@
+package chapter33;
+
+//code.oa.gg/java8/1127
+public class Main {
+    public static void main(String[] args) {
+        HashMap aMap = new HashMap();
+
+        aMap.put(1, "안녕");
+        aMap.put("A", false);
+    }
+}
+
+class HashMap {
+
+    public void put(Object key, Object value) {
+
+    }
+}
