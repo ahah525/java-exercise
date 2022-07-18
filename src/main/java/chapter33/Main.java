5347ac266ae1e989cfedd3ac5f1e5fdf1effd921
ahah525 <ahah525@naver.com>
2022-07-18 11:16:33 +0900

    chapter33.HashMap 만들기2

--- src/main/java/chapter33/Main.java
+++ src/main/java/chapter33/Main.java
@@ -7,12 +7,43 @@ public class Main {
 
         aMap.put(1, "안녕");
         aMap.put("A", false);
+
+        System.out.println(aMap.get("A"));
+        // 출력 : false
     }
 }
 
 class HashMap {
+    private Object keys[];
+    private Object values[];
+    int size;
+
+    public HashMap() {
+        keys = new Object[10];
+        values = new Object[10];
+        size = 0;
+    }
 
     public void put(Object key, Object value) {
+        keys[size] = key;
+        values[size] = value;
+        size++;
+    }
 
+    public Object get(Object key) {
+        for (int i = 0; i < size; i++) {
+            if(keys[i].equals(key)) {
+                return values[i];
+            }
+        }
+        return null;
+    }
+
+    public boolean containsKey(Object key) {
+        for (int i = 0; i < size; i++) {
+            if(keys[i].equals(key))
+                return true;
+        }
+        return false;
     }
 }
