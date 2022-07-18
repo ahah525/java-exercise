5254f07fbe946fa9dd83c246ade9e5039fedad82
ahah525 <ahah525@naver.com>
2022-07-18 15:24:44 +0900

    chapter34.인터페이스-3

--- src/main/java/chapter34/Main.java
+++ src/main/java/chapter34/Main.java
@@ -5,29 +5,54 @@ package chapter34;
 
 class Main {
     public static void main(String[] args) {
-        사람 a사람 = new 홍길동();
-        변호사 a변호사 = (변호사)a사람;
-        치과의사 a치과의사 = (치과의사)a사람;
-        성화봉송자 a성화봉송자 = (성화봉송자)a사람;
+        사람 a사람 = new 사람();
+
+        변호사 a변호사 = a사람;
+        a변호사.변호하다();
+        // 출력 : 사람이 변호 합니다.
+
+        변호사 a변호사2 = new 오랑우탄();
+        a변호사2.변호하다();
+        // 출력 : 오랑우탄이 변호 합니다.
+
+        의사 a의사 = new 오랑우탄();
+        a의사.진찰하다();
+        // 출력 : 오랑우탄이 진찰 합니다.
+
+        의사 a의사2 = new 사람();
+        a의사2.진찰하다();
+        // 출력 : 사람이 진찰 합니다.
     }
 }
 
+class 오랑우탄 implements 변호사, 의사 {
+    @Override
+    public void 변호하다() {
+        System.out.println("오랑우탄이 변호 합니다.");
+    }
 
-abstract class 사람 {
-
+    @Override
+    public void 진찰하다() {
+        System.out.println("오랑우탄이 진찰 합니다.");
+    }
 }
+
+class 사람 implements 변호사, 의사 {
+    @Override
+    public void 변호하다() {
+        System.out.println("사람이 변호 합니다.");
+    }
+
+    @Override
+    public void 진찰하다() {
+        System.out.println("사람이 진찰 합니다.");
+    }
+}
+
 interface 변호사 {
-
+    void 변호하다();
 }
 
-interface 치과의사 {
-
-}
-
-interface 성화봉송자 {
-
-}
-
-class 홍길동 extends 사람 implements 변호사, 치과의사, 성화봉송자 {
-
+interface 의사 {
+    void 진찰하다();
 }
