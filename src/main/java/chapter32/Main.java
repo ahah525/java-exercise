e44cbf21d60474b316eeb8a124de7b5bd37ad302
ahah525 <ahah525@naver.com>
2022-07-19 11:07:46 +0900

    chapter32.제너릭

--- src/main/java/chapter32/Main.java
+++ src/main/java/chapter32/Main.java
@@ -1,32 +1,111 @@
 package chapter32;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기서 `저장소<>` 는 `저장소<Integer>` 가 생략된 표현이다.
-        저장소<Integer> a정수저장소 = new 저장소<>();
-        a정수저장소.set(10);
-        System.out.println(a정수저장소.get() * a정수저장소.get());
-        // 출력 => 100
+        ArrayList<Integer> al = new ArrayList<Integer>();
 
-        저장소<Boolean> a논리저장소 = new 저장소<>();
-        a논리저장소.set(false);
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(40);
 
-        if ( a논리저장소.get() ) {
-            System.out.println("참 : " + a논리저장소.get());
-        }
-        else {
-            System.out.println("거짓 : " + a논리저장소.get());
-        }
+        al.add(50, 1);
+
+        al.remove(0);
+
+        System.out.println(al);
+
+        System.out.println(al.get(3));  //40
+
+        System.out.println(al.indexOf(50));     // 0
+        System.out.println(al.contains(40));    // true
+
+        System.out.println(al.get(3) + al.get(3));  // 80
+
+        ArrayList<Double> al2 = new ArrayList<Double>();
+        al2.add(10.5);
+        al2.add(3.14);
+        System.out.println(al2);
     }
 }
 
-class 저장소<T> {
-    private T data;
+class ArrayList<T> {
+    private Object[] arr;
+    private int size;
 
-    public void set(T data) {
-        this.data = data;
+    public ArrayList() {
+        arr = new Object[2];
+        size = 0;
     }
-    public T get() {
-        return data;
+
+    @Override
+    public String toString() {
+        return "ArrayList{" +
+                "arr=" + Arrays.toString(arr) +
+                ", size=" + size +
+                '}';
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(T value) {
+        add(value, size);
+    }
+
+    public void add(T value, int index) {
+        sizeUp();
+        // 뒤에서부터 땡기기
+        for (int i = size - 1; i >= index; i--) {
+            arr[i + 1] = arr[i];
+        }
+        arr[index] = value;
+        size++;
+    }
+
+    public T get(int index) {
+        return (T) arr[index];
+    }
+
+    public void remove(int index) {
+        // 앞으로 땡기기
+        for (int i = index + 1; i < size; i++) {
+            arr[i - 1] = arr[i];
+        }
+        size--;
+    }
+
+    // 자동 확장
+    public void sizeUp() {
+        // 현재
+        if (size >= arr.length) {
+            // 2배 늘린 배열에 값 복사하기
+            Object[] newArr = new Object[arr.length * 2];
+            for (int i = 0; i < size; i++) {
+                newArr[i] = arr[i];
+            }
+            arr = newArr;
+        }
+    }
+
+    public int indexOf(T value) {
+        for (int i = 0; i < size; i++) {
+            if (((T) arr[i]).equals(value)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean contains(T value) {
+        for (int i = 0; i < size; i++) {
+            if (((T) arr[i]).equals(value)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
