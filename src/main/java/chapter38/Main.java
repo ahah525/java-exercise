319972f9e43d8d6af3eeb569bb41d72f8385c603
ahah525 <ahah525@naver.com>
2022-07-20 10:06:19 +0900

    chapter38.싱글톤 패턴-3

--- src/main/java/chapter38/Main.java
+++ src/main/java/chapter38/Main.java
@@ -1,6 +1,6 @@
 package chapter38;
 
-//code.oa.gg/java8/1075
+//code.oa.gg/java8/1077
 // 문제 : 아래코드가 실행되도록 해주세요.
 
 class Main {
@@ -12,14 +12,40 @@ class Main {
         사람들[3] = 사람.get사람();
         사람들[4] = 사람.get사람();
 
-        System.out.println("실행완료");
+        for ( int i = 0; i < 사람들.length; i++ ) {
+            사람들[i].자기소개();
+        }
+
+		/*
+		// 출력
+		저는 1번째 사람입니다.
+		저는 2번째 사람입니다.
+		저는 3번째 사람입니다.
+		저는 4번째 사람입니다.
+		저는 5번째 사람입니다.
+		*/
     }
 }
 
 class 사람 {
+    private static int lastId;
+    private int id;
+
+    // static 요소 전용 생성자
+    static {
+        lastId = 0;
+    }
+
+    private 사람(int id) {
+        this.id = id;
+    }
 
     public static 사람 get사람() {
-        return new 사람();
+        return new 사람(++lastId);
+    }
+
+    public void 자기소개() {
+        System.out.printf("저는 %d번째 사람입니다.\n", id);
     }
 }
 
