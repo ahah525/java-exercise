382ad3b4b705692020b7562473e491d0e04136a3
ahah525 <ahah525@naver.com>
2022-07-20 10:42:50 +0900

    chapter38.싱글톤 패턴-4

--- src/main/java/chapter38/Main.java
+++ src/main/java/chapter38/Main.java
@@ -1,16 +1,21 @@
 package chapter38;
 
-//code.oa.gg/java8/1077
+//code.oa.gg/java8/1079
 // 문제 : 아래코드가 실행되도록 해주세요.
+// 조건 : 사람 객체의 수는 3을 넘을 수 없습니다.
 
 class Main {
     public static void main(String[] args) {
-        사람[] 사람들 = new 사람[5];
+        사람[] 사람들 = new 사람[7];
         사람들[0] = 사람.get사람();
         사람들[1] = 사람.get사람();
         사람들[2] = 사람.get사람();
         사람들[3] = 사람.get사람();
         사람들[4] = 사람.get사람();
+        사람들[5] = 사람.get사람();
+        사람들[6] = 사람.get사람();
+//        System.out.println(사람들[2]);
+//        System.out.println(사람들[3]);
 
         for ( int i = 0; i < 사람들.length; i++ ) {
             사람들[i].자기소개();
@@ -21,31 +26,42 @@ class Main {
 		저는 1번째 사람입니다.
 		저는 2번째 사람입니다.
 		저는 3번째 사람입니다.
-		저는 4번째 사람입니다.
-		저는 5번째 사람입니다.
+		저는 3번째 사람입니다.
+		저는 3번째 사람입니다.
+		저는 3번째 사람입니다.
+		저는 3번째 사람입니다.
 		*/
     }
 }
 
 class 사람 {
-    private static int lastId;
-    private int id;
+    private static final int 최대_사람수;
+    private static int 현재_사람수;
+    private static 사람 마지막_사람;   // 마지막에 생성된 사람 객체의 리모컨
+
+    private int 번호;
 
     // static 요소 전용 생성자
     static {
-        lastId = 0;
+        최대_사람수 = 3;
+        현재_사람수 = 0;
+        마지막_사람 = null;
     }
 
-    private 사람(int id) {
-        this.id = id;
+    private 사람(int 번호) {
+        this.번호 = 번호;
     }
 
     public static 사람 get사람() {
-        return new 사람(++lastId);
+        if(현재_사람수 < 최대_사람수){
+            사람 사람 = new 사람(++현재_사람수);
+            마지막_사람 = 사람;
+        }
+        return 마지막_사람;
     }
 
     public void 자기소개() {
-        System.out.printf("저는 %d번째 사람입니다.\n", id);
+        System.out.printf("저는 %d번째 사람입니다.\n", 번호);
     }
 }
 
