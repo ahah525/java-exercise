72b41697131008978eefd45ad91005d79e1f8105
ahah525 <ahah525@naver.com>
2022-07-21 11:27:08 +0900

    chapter40.옵저버 패턴-6

--- src/main/java/chapter40/Main.java
+++ src/main/java/chapter40/Main.java
@@ -1,55 +1,55 @@
-package chapter40;
-
-/*
-문제 : 사람 객체의 `안녕`이 `button.click();`에 의해 실행되도록 해주세요.
-조건 : Button에는 `사람` 이라는 언급이 없어야 합니다.
-*/
-class Main {
-    public static void main(String[] args) {
-        Button button = new Button();
-        button.setListener(new 사람());
-        button.click();
-        button.setListener(new 강아지());
-        button.click();
-    }
-}
-
-interface Listener {
-    public void listen(String msg);
-}
-
-class 사람 implements Listener {
-
-    public void 안녕() {
-        System.out.println("사람::안녕!");
-    }
-
-    @Override
-    public void listen(String msg) {
-        안녕();
-    }
-}
-
-class 강아지 implements Listener {
-    public void 안녕() {
-        System.out.println("강아지::안녕!");
-    }
-
-    @Override
-    public void listen(String msg) {
-        안녕();
-    }
-}
-
-// button에 관찰자
-class Button {
-    private Listener listener;
-    public void click() {
-        listener.listen("버튼 클릭됨!");
-    }
-
-    public void setListener(Listener listener) {
-        this.listener = listener;
-    }
-}
-
+//package chapter40;
+//
+///*
+//문제 : 사람 객체의 `안녕`이 `button.click();`에 의해 실행되도록 해주세요.
+//조건 : Button에는 `사람` 이라는 언급이 없어야 합니다.
+//*/
+//class Main {
+//    public static void main(String[] args) {
+//        Button button = new Button();
+//        button.setListener(new 사람());
+//        button.click();
+//        button.setListener(new 강아지());
+//        button.click();
+//    }
+//}
+//
+//interface Listener {
+//    public void listen(String msg);
+//}
+//
+//class 사람 implements Listener {
+//
+//    public void 안녕() {
+//        System.out.println("사람::안녕!");
+//    }
+//
+//    @Override
+//    public void listen(String msg) {
+//        안녕();
+//    }
+//}
+//
+//class 강아지 implements Listener {
+//    public void 안녕() {
+//        System.out.println("강아지::안녕!");
+//    }
+//
+//    @Override
+//    public void listen(String msg) {
+//        안녕();
+//    }
+//}
+//
+//// button에 관찰자
+//class Button {
+//    private Listener listener;
+//    public void click() {
+//        listener.listen("버튼 클릭됨!");
+//    }
+//
+//    public void setListener(Listener listener) {
+//        this.listener = listener;
+//    }
+//}
+//
--- src/main/java/chapter40/Main5.java
+++ src/main/java/chapter40/Main5.java
@@ -17,34 +17,47 @@ class Main {
         aButton.fireClick();
     }
 }
+interface Listener {
+
+    public void onClick();
+}
 class Button {
-    Object listener;
-    public void setClickEventListener(Object listener) {
+    Listener listener;
+    public void setClickEventListener(Listener listener) {
         this.listener = listener;
     }
 
     public void fireClick() {
-        if (listener instanceof 고양이) {
-            ((고양이) listener).clickResponse();
-        } else if (listener instanceof 개) {
-            ((개) listener).clickResponse();
-        } else if (listener instanceof 부엉이) {
-            ((부엉이) listener).clickResponse();
-        }
+        listener.onClick();
     }
 }
 
-class 고양이 {
+class 고양이 implements Listener {
+    @Override
+    public void onClick() {
+        clickResponse();
+    }
+
     public void clickResponse() {
         System.out.println("나(고양이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
     }
 }
-class 개 {
+class 개 implements Listener {
+    @Override
+    public void onClick() {
+        clickResponse();
+    }
+
     public void clickResponse() {
         System.out.println("나(개)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
     }
 }
-class 부엉이 {
+class 부엉이 implements Listener {
+    @Override
+    public void onClick() {
+        clickResponse();
+    }
+
     public void clickResponse() {
         System.out.println("나(부엉이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
     }
