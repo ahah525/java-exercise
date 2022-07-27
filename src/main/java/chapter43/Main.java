2c82461a7c97d59d200a6eb3b8a1c3a4da12f6c9
ahah525 <ahah525@naver.com>
2022-07-27 10:14:12 +0900

    chapter43.내부 익명 클래스 2-4

--- src/main/java/chapter43/Main.java
+++ src/main/java/chapter43/Main.java
@@ -17,15 +17,47 @@ class MainActivity extends Activity {
     public void onCreate() {
         System.out.println("메인 액티비티가 실행되었습니다.");
 
-        View.OnClickListener a; // 이 코드가 실행되도록 해주세요.
+        // 아래 코드가 실행되도록 해주세요.
+        View.OnClickListener a = new 사과();
+        View aButton = new Button(this);
+        aButton.setOnClickListener(a);
+
+        aButton.fireClickEvent();
+        // 출력 : 사과는 버튼이 클릭되었다는 사실을 방금 알았습니다.!
+        // 조건 : 위 문구는 사과 클래스 안의 메서드에 의해서 출력되어야 합니다.
     }
 }
 
-class View {
-    public static interface OnClickListener {
+class Button extends View {
+    public Button(Activity activity) {
 
     }
 }
+
+class 사과 implements View.OnClickListener {
+    @Override
+    public void onClick() {
+        System.out.println("사과는 버튼이 클릭되었다는 사실을 방금 알았습니다.!");
+    }
+}
+
+abstract class View {
+    private OnClickListener onClickListener;
+    public interface OnClickListener {
+        void onClick();
+    }
+
+    // listener 1개 설정
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    public void fireClickEvent() {
+        onClickListener.onClick();
+    }
+}
+
+
 
 
 
