33a78cfbcc3b9ca982a0b0c9ded2722f7489e71c
ahah525 <ahah525@naver.com>
2022-07-27 09:35:22 +0900

    chapter43.내부 익명 클래스 2-2

---
+++ src/main/java/chapter43/Main.java
@@ -0,0 +1,32 @@
+package chapter43;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Activity activity = new MainActivity();
+        activity.onCreate();
+    }
+}
+
+abstract class Activity {
+    abstract public void onCreate();
+}
+
+class MainActivity extends Activity {
+    @Override
+    public void onCreate() {
+        System.out.println("메인 액티비티가 실행되었습니다.");
+
+        View.OnClickListener a; // 이 코드가 실행되도록 해주세요.
+    }
+}
+
+class View {
+    public static interface OnClickListener {
+
+    }
+}
+
+
+
+
